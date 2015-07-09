74a4b0d3cc61af01213046e473f9ddf40f13c467
tyler liu <replicat0r.misc@gmail.com>
2015-07-09 17:32:54 -0400

    pa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
   config.assets.digest = true
   config.assets.precompile << Proc.new { |path|
     if path =~ /\.(css|js)\z/
-      full_path = Rails.application.assets.resolve(path).to_path
-      app_assets_path = Rails.root.join('app', 'assets').to_path
+      full_path = Rails.application.assets.resolve(path).to_s
+      app_assets_path = Rails.root.join('app', 'assets').to_s
       if full_path.starts_with? app_assets_path
         puts "including asset: " + full_path
         true
